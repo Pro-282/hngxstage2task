1d8f1a4a12db633c324249d151cdc4635d2f62e3
Daniel Promise <promise.olayimika.171076@fuoye.edu.ng>
2023-09-14 13:01:08 +0100

    feat:made api accept userName for CRUD operations

--- controllers/PostControllers.go
+++ controllers/PostControllers.go
@@ -1,18 +1,22 @@
 package controllers
 
 import (
-	"log"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/pro-282/hngstage2task/initializers"
 	"github.com/pro-282/hngstage2task/models"
+	"gorm.io/gorm"
 )
 
-func PostCreate(c *gin.Context) {
+func CreateUser(c *gin.Context) {
 	// Get data off req body
 	var body struct{ Name string }
-	c.Bind(&body)
+	if err := c.Bind(&body); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request body, Name must be a string"})
+		return
+	}
 
 	// Generate a new UUID
 	id := uuid.New()
@@ -22,7 +26,7 @@ func PostCreate(c *gin.Context) {
 	result := initializers.DB.Create(&user)
 
 	if result.Error != nil {
-		c.Status(400)
+		c.JSON(400, gin.H{"error": "Failed to create user"})
 		return
 	}
 	// Return it
@@ -39,54 +43,121 @@ func FetchAllUsers(c *gin.Context) {
 
 func FetchUserById(c *gin.Context) {
 	// Get Id from request
-	id := c.Param("id")
+	param := c.Param("id")
 
-	// Get the user
-	var user models.User
-	result := initializers.DB.First(&user, "id = ?", id)
+	// Check if the param is a valid UUID
+	if isValidUUID(param) {
+		// Fetch user by ID
+		var user models.User
+		result := initializers.DB.First(&user, "id = ?", param)
 
-	if result.Error != nil {
-		log.Fatal(result.Error)
-		c.Status(400)
-		return
+		if result.Error != nil {
+			c.JSON(400, gin.H{"error": "User not found"})
+			return
+		}
+
+		c.JSON(200, user)
+	} else {
+		// Fetch user by Username
+		var user models.User
+		result := initializers.DB.First(&user, "name = ?", param)
+
+		if result.Error != nil {
+			c.JSON(400, gin.H{"error": "User not found"})
+			return
+		}
+
+		c.JSON(200, user)
 	}
-
-	// Respond with result
-	c.JSON(200, user)
 }
 
 func UpdateUserName(c *gin.Context) {
-	// Get Id off the url
-	id := c.Param("id")
+	// Get Id or Username from request
+	param := c.Param("id")
 
-	// Get the data off req body
-	var body struct{ Name string }
-	c.Bind(&body)
+	// Check if the param is a valid UUID
+	if isValidUUID(param) {
+		// Update user by ID
+		var user models.User
+		result := initializers.DB.First(&user, "id = ?", param)
 
-	// find the user we are updating
-	var user models.User
-	result := initializers.DB.First(&user, "id = ?", id)
+		if result.Error != nil {
+			c.JSON(400, gin.H{"error": "User not found"})
+			return
+		}
 
-	if result.Error != nil {
-		log.Fatal(result.Error)
-		c.Status(400)
+		// Get the data off req body
+		var body struct{ Name string }
+		if err := c.Bind(&body); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		// Update user's name
+		user.Name = body.Name
+		initializers.DB.Save(&user)
+
+		c.JSON(200, user)
+	} else {
+		// Update user by Username
+		var user models.User
+		result := initializers.DB.First(&user, "name = ?", param)
+
+		if result.Error != nil {
+			c.JSON(400, gin.H{"error": "User not found"})
+			return
+		}
+
+		// Get the data off req body
+		var body struct{ Name string }
+		if err := c.Bind(&body); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		// Update user's name
+		user.Name = body.Name
+		initializers.DB.Save(&user)
+
+		c.JSON(200, user)
 	}
-
-	// update it
-	user.Name = body.Name
-	initializers.DB.Save(&user)
-
-	// Respond with it
-	c.JSON(200, user)
 }
 
 func UserDelete(c *gin.Context) {
-	// Get the id of the post
-	id := c.Param("id")
+	// Get the id or username
+	param := c.Param("id")
 
-	// Delete the post
-	initializers.DB.Delete(&models.User{}, "id = ?", id)
+	var result *gorm.DB
+
+	if isValidUUID(param) {
+		// Delete the User by id
+		result = initializers.DB.Delete(&models.User{}, "id = ?", param)
+	} else {
+		// Delete the User by name
+		result = initializers.DB.Delete(&models.User{}, "name = ?", param)
+	}
+
+	if result.Error != nil {
+		c.JSON(400, gin.H{"error": "Failed to delete user"})
+		return
+	}
+
+	if result.RowsAffected == 0 {
+		c.JSON(404, gin.H{"error": "User not found"})
+		return
+	}
 
 	// Respond
 	c.Status(200)
+}
+
+// Function to check if a string is a valid UUID
+func isValidUUID(s string) bool {
+	// This regex pattern checks for a valid UUID format
+	// Example UUID format: 123e4567-e89b-12d3-a456-426655440000
+	uuidPattern := `^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`
+
+	// Use regular expression to match the pattern
+	match, _ := regexp.MatchString(uuidPattern, s)
+	return match
 }
